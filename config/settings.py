384b3b74bc379458c083876c62fa73340e39b9a9
pakodev <pakodev@yandex.ru>
2023-07-26 19:14:10 +0300

    add corsheaders

--- config/settings.py
+++ config/settings.py
@@ -36,11 +36,13 @@ INSTALLED_APPS = [
     "rest_framework_simplejwt",
     "pets.apps.PetsConfig",
     "api.apps.ApiConfig",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -49,6 +51,9 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = "config.urls"
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_URLS_REGEX = r"^/api/.*$"
 
 TEMPLATES = [
     {
